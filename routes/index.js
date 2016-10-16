51f635ef36b990b521c9c54562525079baa3f1c0
yxc_gdut <yxc_gdut@aliyun.com>
2016-10-16 20:25:05 +0800

    分页

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,26 @@ router.get('/', function(req, res, next) {
 router.get('/news/1', function(req, res, next) {
   res.render('news/1', { title: '艾灸知识大全丨艾艾贴，引领健康养生新时尚' });
 });
+router.get('/aat', function(req, res, next) {
+  res.render('aat', { title: '艾灸知识大全丨艾艾贴，引领健康养生新时尚' });
+});
+router.get('/news', function(req, res, next) {
+  res.render('news', { title: '艾灸知识大全丨艾艾贴，引领健康养生新时尚' });
+});
+router.get('/product', function(req, res, next) {
+  res.render('product', { title: '艾灸知识大全丨艾艾贴，引领健康养生新时尚' });
+});
+router.get('/success', function(req, res, next) {
+  res.render('success', { title: '艾灸知识大全丨艾艾贴，引领健康养生新时尚' });
+});
+router.get('/aijiu', function(req, res, next) {
+  res.render('aijiu', { title: '艾灸知识大全丨艾艾贴，引领健康养生新时尚' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: '艾灸知识大全丨艾艾贴，引领健康养生新时尚' });
+});
+router.get('/join', function(req, res, next) {
+  res.render('join', { title: '艾灸知识大全丨艾艾贴，引领健康养生新时尚' });
+});
 
 module.exports = router;
